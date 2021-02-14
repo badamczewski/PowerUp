5a2cf05e854f6b2192c3835f827195d39845cacc
Bartosz Adamczewski <bartoszadamczewski@gmail.com>
2021-02-14 20:32:43 +0100

    PowerUp: Added Missing files.

---
+++ src/PowerUp.Core/Decompilation/JITExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using PowerUp.Core.Console;
+
+namespace PowerUp.Core.Decompilation
+{
+    public static class JitExtensions
+    {
+        /// <summary>
+        /// Extracts native assembly code from method.
+        /// This method will only provide a tier0 or optimizedTier (for loops) native assembly
+        /// when JIT Tiered compilation is on.
+        ///
+        /// For optimized build you should build your project with:  <TieredCompilation>false</TieredCompilation>
+        /// </summary>
+        /// <param name="methodInfo"></param>
+        /// <returns></returns>
+        public static DecompiledMethod ToAsm(this MethodInfo methodInfo)
+        {
+            RuntimeHelpers.PrepareMethod(methodInfo.MethodHandle);
+            var decompiler = new JitCodeDecompiler();
+            var decompiled = decompiler.DecompileMethod(methodInfo);
+
+            return decompiled;
+        }
+
+        /// <summary>
+        /// Extracts native assembly code from method.
+        /// This method will provide a tier0 and tier1 native code but in order to do that
+        /// the user needs to run the method a sufficient number of times; That's why
+        /// the action delegate is provided.
+        /// </summary>
+        /// <param name="methodInfo"></param>
+        /// <returns></returns>
+        public static (DecompiledMethod tier0, DecompiledMethod tier1)
+            ToAsm(this MethodInfo methodInfo, Action action)
+        {
+            RuntimeHelpers.PrepareMethod(methodInfo.MethodHandle);
+            var decompiler = new JitCodeDecompiler();
+            var decompiled = decompiler.DecompileMethod(methodInfo);
+
+            int runCount = 10_000_000;
+            int checkCount = 100;
+
+            for (int i = 0; i < checkCount; i++)
+            {
+                for (int n = 0; n < runCount; n++)
+                {
+                    action();
+                }
+
+                var decompiledAfter = decompiler.
+                    DecompileMethod(
+                        decompiled.CodeAddress,
+                        decompiled.CodeSize,
+                        methodInfo.Name);
+
+                if (decompiledAfter.CodeAddress != decompiled.CodeAddress)
+                {
+                    return (decompiled, decompiledAfter);
+                }
+            }
+
+            return (decompiled, decompiled);
+        }
+        
+        /// <summary>
+        /// Prints assembly code of the decompiled method.
+        /// </summary>
+        /// <param name="method"></param>
+        public static void Print(this DecompiledMethod method)
+        {
+            XConsole.WriteLine(method.ToString());
+        }
+
+    }
+
+}
