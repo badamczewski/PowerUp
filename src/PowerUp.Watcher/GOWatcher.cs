c111276a7577d661817e0da30f7f58e971904d71
badamczewski <bartoszadamczewski@gmail.com>
2021-11-16 23:45:25 +0100

    PowerUp: Go Jump Guides added, COOOOL :)

--- src/PowerUp.Watcher/GOWatcher.cs
+++ src/PowerUp.Watcher/GOWatcher.cs
@@ -31,6 +31,34 @@ namespace PowerUp.Watcher
 {
     public class GOWatcher
     {
+        private string[] jumpInstructions = new string[] {
+
+            //
+            // Conditional Jumps
+            //
+            "JCC",
+            "JCS",
+            "JCXZL",
+            "JEQ",
+            "JGE",
+            "JGT",
+            "JHI",
+            "JLE",
+            "JLS",
+            "JLT",
+            "JMI",
+            "JNE",
+            "JOC",
+            "JOS",
+            "JPC",
+            "JPL",
+            "JPS",
+            //
+            // Unconditional Jumps
+            //
+            "JMP"
+        };
+    
         private string _pathToGOCompiler;
         private IConfigurationRoot _configuration;
 
@@ -70,7 +98,8 @@ namespace PowerUp.Watcher
         {
             Initialize(goFile, outAsmFile);
 
-            var command = $"{_pathToGOCompiler}go.exe tool compile -S {goFile} > {outAsmFile}";
+            var tmpAsmFile = outAsmFile + "_tmp.asm";
+            var command = $"{_pathToGOCompiler}go.exe tool compile -S {goFile} > {tmpAsmFile}";
             string lastCode = null;
             DateTime lastWrite = DateTime.MinValue;
             var iDontCareAboutThisTask = Task.Run(async () =>
@@ -97,6 +126,12 @@ namespace PowerUp.Watcher
                                 process.WaitForExit();
 
                                 lastCode = code;
+
+                                var methods = ParseASM(File.ReadAllText(tmpAsmFile));
+                                var asmCode = ToAsmString(new DecompilationUnit() { DecompiledMethods = methods });
+
+                                File.WriteAllText(outAsmFile, asmCode);
+
                             }
                         }
                     }
@@ -114,5 +149,340 @@ namespace PowerUp.Watcher
             });
             return iDontCareAboutThisTask;
         }
+
+        public string ToAsmString(DecompilationUnit unit)
+        {
+            StringBuilder builder = new StringBuilder();
+            StringBuilder lineBuilder = new StringBuilder();
+
+            foreach (var method in unit.DecompiledMethods)
+            {
+                if (method == null) continue;
+
+                (int jumpSize, int nestingLevel) sizeAndNesting = (-1, -1);
+                sizeAndNesting = JumpGuideDetector.PopulateGuides(method);
+                //
+                // Print messages.
+                //
+                if (method.Messages != null && method.Messages.Length > 0)
+                {
+                    builder.AppendLine(
+                        Environment.NewLine +
+                        string.Join(Environment.NewLine, method.Messages));
+                }
+
+                builder.AppendLine($"# Instruction Count: {method.Instructions.Count}; Code Size: {method.CodeSize}");
+                builder.Append($"{method.Return} {method.Name}(");
+
+                for (int i = 0; i < method.Arguments.Length; i++)
+                {
+                    builder.Append($"{method.Arguments[i]}");
+
+                    if (i != method.Arguments.Length - 1)
+                    {
+                        builder.Append(", ");
+                    }
+                }
+
+                builder.AppendLine("):");
+
+                int pad = 6;
+                foreach (var inst in method.Instructions)
+                {
+                    lineBuilder.Clear();
+
+                    //
+                    // @TODO This is a temp solution to a tailing binary dump of the 
+                    // method that we should not even parse.
+                    //
+                    if (inst.Instruction == null) continue;
+
+                    var offset = pad - inst.Instruction.Length;
+                    if (offset < 0) offset = 0;
+
+                    var hexAddrSize = inst.Address.ToString("X");
+                    int hexSize = 4;
+
+                    string hexPad = new string('0', hexSize - hexAddrSize.Length);
+
+                    lineBuilder.Append("  ");
+
+                    AppendGuides(lineBuilder, inst, sizeAndNesting);
+                    lineBuilder.Append($"{hexPad}{hexAddrSize}: ");
+                    lineBuilder.Append($"{inst.Instruction} " + new string(' ', offset));
+
+                    int idx = 0;
+                    foreach (var arg in inst.Arguments)
+                    {
+                        var argumentValue = CreateArgument(method.CodeAddress, method.CodeSize, inst, arg, idx == inst.Arguments.Length - 1, unit.Options);
+                        lineBuilder.Append(argumentValue);
+                        idx++;
+                    }
+
+                    builder.Append(lineBuilder.ToString());
+                    builder.AppendLine();
+                }
+
+                builder.Append(lineBuilder.ToString());
+                builder.AppendLine();
+            }
+
+            return builder.ToString();
+        }
+
+        private void AppendGuides(StringBuilder builder, AssemblyInstruction inst, (int jumpSize, int nestingLevel) sizeAndNesting)
+        {
+            int wsCount = 0;
+            bool usedGuides = false;
+            for (int i = 0; i <= sizeAndNesting.nestingLevel; i++)
+            {
+                var block = inst.GuideBlocks[i];
+
+                //
+                // Guide not found, append whitespace.
+                // @TODO: Most of this stuff is done when we're populating guides
+                // This loop should be as simple as possible.
+                //
+                if (block == '\0') wsCount++;
+                else
+                {
+                    if (wsCount > 0) builder.Append(new String(' ', wsCount));
+                    wsCount = 0;
+                    builder.Append((char)block);
+                    usedGuides = true;
+                }
+            }
+
+            if (sizeAndNesting.nestingLevel > 0 && usedGuides == false)
+                builder.Append(' ', sizeAndNesting.nestingLevel);
+        }
+
+        private string CreateArgument(ulong methodAddress, ulong codeSize, AssemblyInstruction instruction, InstructionArg arg, bool isLast, Core.Compilation.CompilationOptions options)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            if (instruction.jumpDirection != JumpDirection.None)
+            {
+                var addressInArg = arg.Value.LastIndexOf(' ');
+                var value = arg.Value;
+                if (addressInArg != -1)
+                {
+                    value = arg.Value.Substring(0, addressInArg);
+                }
+
+                var valueFormatted = ulong.Parse(value.Trim()).ToString("X");
+                builder.Append($"{valueFormatted}");
+
+                if (instruction.jumpDirection == JumpDirection.Out)
+                    builder.Append($" ↷");
+                else if (instruction.jumpDirection == JumpDirection.Up)
+                    builder.Append($" ⇡");
+                else if (instruction.jumpDirection == JumpDirection.Down)
+                    builder.Append($" ⇣");
+            }
+            else
+            {
+                //Parse Value
+                var value = arg.Value.Trim();
+                var code = string.Empty;
+                for (int i = 0; i < value.Length; i++)
+                {
+                    var c = value[i];
+                    if (c == ']' || c == '[' || c == '+' || c == '-' || c == '*')
+                    {
+                        if (string.IsNullOrEmpty(code) == false)
+                        {
+                            builder.Append($"{code}");
+                            code = string.Empty;
+                        }
+
+                        builder.Append($"{c}");
+                    }
+                    else
+                    {
+                        code += c;
+                    }
+                }
+                if (string.IsNullOrEmpty(code) == false)
+                {
+                    builder.Append($"{code}");
+                }
+            }
+
+            if (isLast == false)
+            {
+                builder.Append($", ");
+            }
+
+            return builder.ToString();
+        }
+
+        private DecompiledMethod[] ParseASM(string asm)
+        {
+            //
+            // GO ASM Format can be expressed by a state machine going line by line
+            // We have multiple types of lines but for methods we are interested
+            // in two types of lines:
+            // - Header Lines
+            // - Instruction Lines
+            // This is a header line:
+            //     "".M STEXT nosplit size=17 args=0x8 locals=0x0 funcid=0x0
+            //
+            // This is a instruction line:
+            //     0x0006 00006 (_code.go:23)	MOVL $1, AX
+            //
+            // There's a special instruction called TEXT that contains
+            // mostly the same info as the header line.
+            // 
+            // Let's parse instruction lines for now (an instruction line will always contain an address)
+            // since this should give us all of the information that we need.
+            //
+            var lines = asm
+                .Trim()
+                .Split("\n");
+
+            List<DecompiledMethod> methods = new List<DecompiledMethod>();
+            DecompiledMethod decompiledMethod = null;
+            int index = 0;
+
+            for (int i = 0; i < lines.Length; i++)
+            { 
+                var line = lines[i];
+                line = line.Trim();
+
+                if (line.StartsWith("0x"))
+                {
+                    if (line.Contains("TEXT"))
+                    {
+                        var nameInst = ParseInstruction(line);
+
+                        index = 0;
+                        decompiledMethod = new DecompiledMethod();
+                        decompiledMethod.Name = nameInst.Arguments[0].Value;
+                        decompiledMethod.Arguments = new string[0];
+                        methods.Add(decompiledMethod);
+                    }
+                    else
+                    {
+                        var inst = ParseInstruction(line);
+                        inst.OrdinalIndex = index;
+                        index++;
+                        decompiledMethod.Instructions.Add(inst);
+                        //
+                        // @TODO this has off by X errors.
+                        // We need to parse header lines and read the correct
+                        // value(s) from there. Remove as soon as more pressing issues
+                        // are dealth with.
+                        //
+                        var size = (uint)inst.Address;
+                        if(decompiledMethod.CodeSize < size)
+                            decompiledMethod.CodeSize = size;
+
+                    }
+                }
+            }
+
+            return methods.ToArray();
+        }
+
+
+        private AssemblyInstruction ParseInstruction(string line)
+        {
+            XConsoleTokenizer tokenizer = new XConsoleTokenizer();
+            var tokens = tokenizer.Tokenize(line);
+            //
+            // Instruction Layout:
+            // (Whitespaces and tabs are skiped in this diagram but they are tokenized
+            // and should be handeled).
+            //
+            //  
+            //     [0]        [2]    [4]     [5]      [6]     [8]
+            // "Address0" "Address1" "(" "PathToFile" ")" "Instruction" "Arg1", "ArgX"
+            // 
+            // All of the index offsets arent guaranteed except the first two, but what we
+            // can expect that after address tokens we should expect "(" at some point and skip 
+            // this whole section, and after this section we should expect an instruction followed
+            // by a list of arguments.
+            //
+            // An argument can have operators, parens, numbers, strings.
+            // Arguments in the list will be seperated by a comma ',' 
+            //
+            
+            AssemblyInstruction instruction = new AssemblyInstruction();
+            instruction.Address = HexToDecimal(tokens[0].GetValue());
+            //
+            // The OpCode should be after ")"
+            //
+            int argsIndex = -1;
+            for(int i = 3; i < tokens.Count; i++)
+            {
+                if(tokens[i] is BracketCloseToken && i + 1 < tokens.Count)
+                {
+                    while(PeekNext(tokens, i, out var next))
+                    {
+                        if(next is WordToken word)
+                        {
+                            instruction.Instruction = tokens[i + 1].GetValue();
+                            //
+                            // Skip over whitespace after the instruction.
+                            //
+                            argsIndex = i + 3;
+                            break;
+                        }
+                        i++;
+                    }
+                    break;
+                }
+            }
+
+            List<InstructionArg> instructionArgs = new List<InstructionArg>();
+
+            if (argsIndex != -1 && argsIndex < tokens.Count)
+            {
+                InstructionArg instructionArg = new InstructionArg();
+
+                for (; argsIndex < tokens.Count; argsIndex++)
+                {
+                    if (tokens[argsIndex] is SeparatorToken)
+                    {
+                        instructionArgs.Add(instructionArg);
+                        instructionArg = new InstructionArg();
+                    }
+                    else
+                    {
+                        instructionArg.Value += tokens[argsIndex].GetValue();
+                        if (jumpInstructions.Contains(instruction.Instruction))
+                        {
+                            instruction.RefAddress = ulong.Parse(instructionArg.Value); 
+                            instructionArg.HasReferenceAddress = true;
+                        }
+                    }
+                }
+
+                instructionArgs.Add(instructionArg);
+            }
+
+            instruction.Arguments = instructionArgs.ToArray();
+            return instruction;
+        }
+
+        private ulong HexToDecimal(string value)
+        {
+            var hexEssence = value.Substring(0, value.Length);
+            return Convert.ToUInt64(hexEssence, 16);
+        }
+
+        private bool PeekNext(List<Token> tokens, int i, out Token next)
+        {
+            next = null;
+            if (i + 1 < tokens.Count)
+            {
+                next = tokens[i + 1];
+                return true;
+            }
+            return false;
+        }
     }
+
+
 }
---
+++ src/PowerUp.Watcher/JumpGuideDetector.cs
@@ -0,0 +1,252 @@
+﻿using PowerUp.Core.Decompilation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PowerUp.Core.Console.XConsole;
+
+namespace PowerUp.Watcher
+{
+    /// <summary>
+    /// Generic class that will be used to detect and set jump guides.
+    /// To be able to set Guides for any decompilation type you need the following:
+    /// 
+    /// 1. The method needs a known address and code size.
+    /// 2. Instruction needs to have a  'RefAddress' set.
+    /// 3. Instruction Argument needs a 'HasReferenceAddress' flag set.
+    /// 
+    /// </summary>
+    public class JumpGuideDetector
+    {
+        //
+        // This section describes guides drawing.
+        //
+        // When presented with the following code:
+        //
+        // 001: A
+        // 002: JMP 005
+        // 003: B
+        // 004: JMP 005
+        // 005: C
+        // 006: D
+        // 007: JMP 001
+        //
+        // We need to produce the following guides:
+        //
+        // ┌001: A
+        // │┌002: JMP 005
+        // ││003: B
+        // ││┌004: JMP 005
+        // │└└005: C
+        // │006: D
+        // └007: JMP 001
+        //
+        // To make this plesant to look at the longest jumps should be the most outer ones.
+        // 1. We need to figure out the direction of each jump, and see if the jump is inside our method.
+        // 2. We need to compute the lenght of each jump.
+        // 3. We then sort the jump table by the longest length
+        // 4. We draw the guides.
+        //
+        // Since we're writing out one instruction at a time from top to bottom we will need an additional piece of information
+        // on the instruction or as a lookup table that will tell us if we should draw a line segment(s), for example:
+        // When we are writing out 005: C we need to lookup the guides table that will contain:
+        //   005 =>
+        //      [0] = |
+        //      [1] = └
+        //      [2] = └
+        // 
+        public static (int jumpSize, int nestingLevel) PopulateGuides(DecompiledMethod method)
+        {
+            if (method == null || method.Instructions.Any() == false) return (0, 0);
+
+            var methodAddress = method.CodeAddress;
+            var codeSize = method.CodeSize;
+            int jumps = 0;
+
+            foreach (var instruction in method.Instructions)
+            {
+                foreach (var arg in instruction.Arguments)
+                {
+                    if (arg.HasReferenceAddress && instruction.RefAddress > 0)
+                    {
+                        instruction.jumpDirection = JumpDirection.Out;
+                        if (instruction.RefAddress >= methodAddress && instruction.RefAddress <= methodAddress + codeSize)
+                        {
+                            jumps++;
+                            //
+                            //  Jump Up
+                            //
+                            instruction.jumpDirection = JumpDirection.Up;
+                            if (instruction.Address < instruction.RefAddress)
+                            {
+                                //
+                                // Jump Down
+                                //
+                                instruction.jumpDirection = JumpDirection.Down;
+                            }
+                            //
+                            // Find the instruction and relative distance
+                            //
+                            foreach (var jmpTo in method.Instructions)
+                            {
+                                if (instruction.RefAddress == jmpTo.Address)
+                                {
+                                    //Found.
+                                    instruction.JumpIndex = jmpTo.OrdinalIndex;
+                                    instruction.JumpSize = Math.Abs(instruction.OrdinalIndex - jmpTo.OrdinalIndex);
+
+                                    //
+                                    // Can only jump to one target
+                                    //
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            //
+            // Most outer instruction -> most inner instruction
+            //
+            int index = 0;
+            int maxJmpSize = -1;
+
+            var orderedInstructions = method.Instructions.OrderByDescending(x => x.JumpSize).ToArray();
+            maxJmpSize = orderedInstructions[0].JumpSize;
+
+            foreach (var orderedInstruction in orderedInstructions)
+            {
+                if (orderedInstruction.jumpDirection == JumpDirection.Out || orderedInstruction.jumpDirection == JumpDirection.None)
+                    continue;
+
+                var inst = method.Instructions[orderedInstruction.OrdinalIndex];
+
+                if (inst.jumpDirection == JumpDirection.Down)
+                {
+                    PopulateGuidesForDownJump(inst, method, jumps, index);
+                }
+                else if (inst.jumpDirection == JumpDirection.Up)
+                {
+                    PopulateGuidesForUpJump(inst, method, jumps, index);
+                }
+
+                index += 2;
+            }
+
+            return (maxJmpSize, index);
+        }
+
+        private static void PopulateGuidesForDownJump(AssemblyInstruction inst, DecompiledMethod method, int methodJumpCount, int nestingIndex)
+        {
+            //
+            // What is our maximum nesting level for this jump.
+            //
+            var level = 2 * methodJumpCount - nestingIndex;
+
+            //
+            // Generate starting guides 
+            //
+            inst.GuideBlocks[nestingIndex] = ConsoleBorderStyle.TopLeft;
+            for (int i = 1; i < level - 1; i++)
+                inst.GuideBlocks[nestingIndex + i] = ConsoleBorderStyle.TopBottom;
+
+            inst.GuideBlocks[nestingIndex + level - 1] = ConsoleBorderStyle.Bullet;
+
+
+            for (int i = 1; i < inst.JumpSize; i++)
+            {
+                var nestedInst = method.Instructions[inst.OrdinalIndex + i];
+
+                //
+                // Check prev guide and if the guide is TopBotom char then 
+                // we change our guide to a plus.
+                //
+                if (nestingIndex > 0 && nestedInst.GuideBlocks[nestingIndex - 1] == ConsoleBorderStyle.TopBottom)
+                    nestedInst.GuideBlocks[nestingIndex] = ConsoleBorderStyle.SeparatorBoth;
+                else
+                    nestedInst.GuideBlocks[nestingIndex] = ConsoleBorderStyle.Left;
+
+                //
+                // Populate everything down with whitespace.
+                //
+                for (int l = 1; l < level; l++)
+                {
+                    if (nestedInst.GuideBlocks[nestingIndex + l] == '\0')
+                        nestedInst.GuideBlocks[nestingIndex + l] = ' ';
+                }
+            }
+
+            //
+            // Get last instruction to set the arrow.
+            //
+            var lastInst = method.Instructions[inst.OrdinalIndex + inst.JumpSize];
+            //
+            // If guide above me is TopBotom then change it to arrow since, we are ending a jump here;
+            // So someone jumps here as well.
+            //
+            if (nestingIndex > 0 && lastInst.GuideBlocks[nestingIndex - 1] == ConsoleBorderStyle.TopBottom)
+                lastInst.GuideBlocks[nestingIndex - 1] = '>';
+
+            //
+            // Generate ending guides 
+            //
+            lastInst.GuideBlocks[nestingIndex] = ConsoleBorderStyle.BottomLeft;
+            for (int i = 1; i < level - 1; i++)
+                lastInst.GuideBlocks[nestingIndex + i] = ConsoleBorderStyle.TopBottom;
+
+            lastInst.GuideBlocks[nestingIndex + level - 1] = '>';
+        }
+        private static void PopulateGuidesForUpJump(AssemblyInstruction inst, DecompiledMethod method, int methodJumpCount, int nestingIndex)
+        {
+            //
+            // What is our maximum nesting level for this jump.
+            //
+            var level = 2 * methodJumpCount - nestingIndex;
+
+            //
+            // Generate starting guides 
+            //
+            inst.GuideBlocks[nestingIndex] = ConsoleBorderStyle.BottomLeft;
+            for (int i = 1; i < level - 1; i++)
+                inst.GuideBlocks[nestingIndex + i] = ConsoleBorderStyle.TopBottom;
+
+            inst.GuideBlocks[nestingIndex + level - 1] = ConsoleBorderStyle.Bullet;
+
+
+            for (int i = 1; i < inst.JumpSize; i++)
+            {
+                var nestedInst = method.Instructions[inst.OrdinalIndex - i];
+
+                //
+                // Check prev guide and if the guide is TopBotom char then 
+                // we change our guide to a plus.
+                //
+                if (nestingIndex > 0 && nestedInst.GuideBlocks[nestingIndex - 1] == ConsoleBorderStyle.TopBottom)
+                    nestedInst.GuideBlocks[nestingIndex] = ConsoleBorderStyle.SeparatorBoth;
+                else
+                    nestedInst.GuideBlocks[nestingIndex] = ConsoleBorderStyle.Left;
+
+                //
+                // Populate everything down with whitespace.
+                //
+                for (int l = 1; l < level; l++)
+                {
+                    if (nestedInst.GuideBlocks[nestingIndex + l] == '\0')
+                        nestedInst.GuideBlocks[nestingIndex + l] = ' ';
+                }
+            }
+
+            //
+            // Generate ending guides 
+            //
+            var lastInst = method.Instructions[inst.OrdinalIndex - inst.JumpSize];
+            lastInst.GuideBlocks[nestingIndex] = ConsoleBorderStyle.TopLeft;
+            for (int i = 1; i < level - 1; i++)
+                lastInst.GuideBlocks[nestingIndex + i] = ConsoleBorderStyle.TopBottom;
+
+            lastInst.GuideBlocks[nestingIndex + level - 1] = '>';
+        }
+    }
+}
